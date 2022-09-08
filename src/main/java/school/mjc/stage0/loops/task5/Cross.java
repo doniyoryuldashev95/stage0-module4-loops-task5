f6f713d9812af40af353ad433c5fc15df28f6baa
doniyoryuldashev95 <doniyoryuldashev95@gmail.com>
2022-09-08 20:41:49 +0500

    newPush

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -3,5 +3,17 @@ package school.mjc.stage0.loops.task5;
 public class Cross {
     public void printCross(int sideLength) {
 
+        for (int i = 1; i <= sideLength; i++) {
+            for (int j = 1; j <= sideLength; j++) {
+                if ((sideLength/2)+1==i){
+                    System.out.print("8");
+                } else if ((sideLength/2)+1==j){
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,7 +1,39 @@
 package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
+    public static void main(String[] args) {
+        int height = 5;
+        StringBuilder s;
+        for (int i = 0; i < height; i++) {
+            int m = (height/2 > i)? i:height-i-1;
+            s = new StringBuilder();
+            for (int j = 1; j <= m; j++) {
+                s.append(" ");
+            }
+            for (int j = 1; j <= height-2*m; j++) {
+                s.append("8");
+            }
+            for (int j = 1; j <= m; j++) {
+                s.append(" ");
+            }
+            System.out.println(s);
+        }
+    }
     public void printHourglassOfGivenSize(int height) {
-
+        StringBuilder s;
+        for (int i = 0; i < height; i++) {
+            int m = (height/2 > i)? i:height-i-1;
+            s = new StringBuilder();
+            for (int j = 1; j <= m; j++) {
+                s.append(" ");
+            }
+            for (int j = 1; j <= height-2*m; j++) {
+                s.append("8");
+            }
+            for (int j = 1; j <= m; j++) {
+                s.append(" ");
+            }
+            System.out.println(s);
+        }
     }
 }
